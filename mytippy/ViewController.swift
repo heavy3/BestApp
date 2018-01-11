b2b5e647bdb57c8bc4e66b5fc3eb287cf0b6c301
suky zhen <su.ky.123@hotmai.com>
2018-01-10 22:33:44 -0800

    Initial Commit

---
+++ mytippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mytippy
+//
+//  Created by suky zhen on 1/10/18.
+//  Copyright © 2018 suky zhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
